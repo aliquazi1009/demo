9dac197fd42387d1d45b71e5afc107113a85dc49
Akbar Ali Quazi <54813737+aliquazi1009@users.noreply.github.com>
2020-12-06 23:03:42 +0530

    Add files via upload

---
+++ js/product-slider.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.product-slider-inner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2670,
+        arrows: false,
+        dots: false,
+        pauseOnHover: true,
+        responsive: [
+        {
+            breakpoint: 991,
+            settings: 
+            {
+                slidesToShow: 3
+            }
+        }, 
+        {
+            breakpoint: 768,
+            settings: 
+            {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 520,
+            settings: 
+            {
+                slidesToShow: 1
+            }
+        }, ]
+    });
+});
+
